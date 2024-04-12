b491a3651b9acae59350dd557cf9f1b8457f3fa4
louis <louis.chopinet@epitech.eu>
2024-04-12 11:59:13 +0200

    [+] Login [~] nft burn, wallet load and balance showing are in different files

---
+++ app/components/ShowBalance.jsx
@@ -0,0 +1,9 @@
+import { useBalance } from '@nice-xrpl/react-xrpl';
+
+export default function ShowBalance() {
+  const balance = useBalance();
+
+  return (
+    <h1 className="text-4xl font-bold">{ balance } XRP</h1>
+  );
+}

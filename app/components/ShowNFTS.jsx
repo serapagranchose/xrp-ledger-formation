21781276037db24852eebd6757aa9e109a1097aa
Charles <charles.dupont@epitech.eu>
2024-04-13 00:31:13 +0200

    feat: profile style

--- app/components/ShowNFTS.jsx
+++ app/components/ShowNFTS.jsx
@@ -49,127 +49,131 @@ export default function ShowNFTS({ tokens, getAllNFTS, userSeed }) {
 
   return (
     <>
-    <h1 className="text-4xl font-bold">NFTs ({tokens?.length}) :</h1>
-    <div className="flex flex-row space-y-2 bg-gray-100 space-around m-4 flex-wrap center">
-      {tokens?.length ?
-      tokens.map((token) => (
-          <div className="items-center flex flex-col space-y-4 m-4" key={token.NFTokenID}>
-            <img
-              src={getSource(token.URI).split('|')[1]}
-              className="w-64 h-64"
-              alt="ipfs image"
-            />
-            <div className="flex flex-row space-x-4 items-center mr-2 ml-2">
-              <div className='flex flex-col'>
-                <h1 className="text-xl font-bold">Name : {getSource(token.URI).split('|')[0]}</h1>
-                <button className='bg-black text-white p-4 rounded-lg' onClick={() => {
-                  navigator.clipboard.writeText(token.NFTokenID);
-                }}>
-                  Copy ID
+      <h1 className="text-4xl font-bold">NFTs ({tokens?.length}) :</h1>
+      <div className="flex flex-row space-y-2 bg-gray-100 space-around m-4 flex-wrap center">
+        {tokens?.length ?
+          tokens.map((token) => (
+            <div className="items-center flex flex-col space-y-4 m-4" key={token.NFTokenID}>
+              <img
+                src={getSource(token.URI).split('|')[1]}
+                className="w-64 h-64"
+                alt="ipfs image"
+              />
+              <div className="flex flex-row space-x-4 items-center mr-2 ml-2">
+                <div className='flex flex-col'>
+                  <h1 className="text-xl font-bold">Name : {getSource(token.URI).split('|')[0]}</h1>
+                  <button className='bg-black text-white p-4 rounded-lg' onClick={() => {
+                    navigator.clipboard.writeText(token.NFTokenID);
+                  }}>
+                    Copy ID
+                  </button>
+                </div>
+                <BurnNFT getAllNFTS={getAllNFTS} client={client} userWallet={userWallet} id={token.NFTokenID} uri={getSource(token.URI).split('|')[1]?.substring(getSource(token.URI).split('|')[1].length - 46)} />
+                <button
+                  className="bg-black text-white p-4 rounded-lg h-12 m-4"
+                  onClick={() => {
+                    setIsOpen(true);
+                    setTokenId(token.NFTokenID)
+                  }}
+                >
+                  Sell NFT
                 </button>
               </div>
-              <BurnNFT getAllNFTS={getAllNFTS} client={client} userWallet={userWallet} id={token.NFTokenID} uri={getSource(token.URI).split('|')[1]?.substring(getSource(token.URI).split('|')[1].length - 46)} />
+            </div>
+          ))
+          : <h1 className="text-2xl font-bold">No NFTs found...</h1>}
+      </div>
+      {isOpen && (
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+          <div className="bg-white p-8 rounded-lg">
+            <h1 className="text-4xl font-bold">Sell NFT</h1>
+            <div className="flex flex-col space-y-4">
+              <input className="outline-none border-2 border-gray-300 rounded-md" type="text" placeholder="Price (drops)" value={price} onChange={(e) => { setPrice(e.currentTarget.value) }} />
               <button
-              className="bg-black text-white p-4 rounded-lg h-12 m-4"
-              onClick={() => {
-                setIsOpen(true);
-                setTokenId(token.NFTokenID)
-              }}
+                className="bg-black text-white p-4 rounded-lg"
+                onClick={async () => {
+                  setCreatingSellOffer(true);
+                  try {
+                    const tx = {
+                      "TransactionType": "NFTokenCreateOffer",
+                      "Account": process.env.NEXT_PUBLIC_BROKER_ADRESS,
+                      "Amount": '2',
+                      "NFTokenID": tokenId,
+                      "Owner": userWallet.classicAddress,
+                    }
+                    const submitted_tx = await client.submitAndWait(tx, {
+                      autofill: true,
+                      wallet: brokerWallet,
+                    })
+                    console.log('Create Offer : ', submitted_tx);
+                    setBuyOffer(submitted_tx.result.meta.offer_id);
+                  } catch (error) {
+                    console.log(error);
+                  } finally {
+                    setBuyOfferCreated(true);
+                    setCreatingSellOffer(false);
+                    setIsOpen(false);
+                  }
+                }}
               >
-                Sell NFT
+                {creatingSellOffer ? (<div><span className="inline-block h-3 w-3 animate-spin rounded-full mb-0.5 mr-1 border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+                  role="status">
+                </span>Creating...</div>) : 'Create Sell Offer'}
               </button>
             </div>
           </div>
-      ))
-      : <h1 className="text-2xl font-bold">No NFTs found...</h1>}
-    </div>
-    {isOpen && (
-      <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
-        <div className="bg-white p-8 rounded-lg">
-          <h1 className="text-4xl font-bold">Sell NFT</h1>
-          <div className="flex flex-col space-y-4">
-            <input type="text" placeholder="Price (drops)" value={price} onChange={(e) => {setPrice(e.currentTarget.value)}} />
+          <button className="absolute top-4 right-4" onClick={() => setIsOpen(false)}>X</button>
+        </div>
+      )}
+      {buyOfferCreated && (
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+          <div className="flex flex-col bg-white p-8 rounded-lg space-y-4">
+            <h1 className="text-4xl font-bold">Accept Buy Offer</h1>
             <button
-            className="bg-black text-white p-4 rounded-lg"
-            onClick={async () => {
-              setCreatingSellOffer(true);
-              try {
-                const tx = {
-                  "TransactionType": "NFTokenCreateOffer",
-                  "Account": process.env.NEXT_PUBLIC_BROKER_ADRESS,
-                  "Amount": '2',
-                  "NFTokenID": tokenId,
-                  "Owner": userWallet.classicAddress,
+              className="bg-black text-white p-4 rounded-lg"
+              onClick={async () => {
+                setAccepting(true);
+                console.log('Buy Offer : ', buyOffer)
+                try {
+                  const tx = {
+                    "TransactionType": "NFTokenAcceptOffer",
+                    "Account": userWallet.classicAddress,
+                    "NFTokenBuyOffer": buyOffer,
+                  }
+                  const submitted_tx = await client.submitAndWait(tx, {
+                    autofill: true,
+                    wallet: userWallet,
+                  })
+                  console.log('Accept Offer : ', submitted_tx);
+                } catch (error) {
+                  console.log(error);
+                } finally {
+                  try {
+                    await createUserSellOffer();
+                  } catch (error) {
+                    console.log(error);
+                  } finally {
+                    setPrice('');
+                  }
+                  setAccepting(false);
+                  setBuyOfferCreated(false);
+                  getAllNFTS({ address: userWallet?.classicAddress });
                 }
-                const submitted_tx = await client.submitAndWait(tx, {
-                  autofill: true,
-                  wallet: brokerWallet,
-                })
-                console.log('Create Offer : ', submitted_tx);
-                setBuyOffer(submitted_tx.result.meta.offer_id);
-              } catch (error) {
-                console.log(error);
-              } finally {
-                setBuyOfferCreated(true);
-                setCreatingSellOffer(false);
-                setIsOpen(false);
-              }
-            }}
+              }}
             >
-              {creatingSellOffer ? 'Creating...' : 'Create Sell Offer'}
+              {accepting ? (<div><span className="inline-block h-3 w-3 animate-spin rounded-full mb-0.5 mr-1 border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+                role="status">
+              </span>Accepting...</div>) : 'Accept the Buy'}
+            </button>
+            <button
+              className="bg-red-500 text-white p-4 rounded-lg"
+              onClick={() => setBuyOfferCreated(false)}
+            >
+              Cancel
             </button>
           </div>
         </div>
-        <button className="absolute top-4 right-4" onClick={() => setIsOpen(false)}>X</button>
-      </div>
-    )}
-    {buyOfferCreated && (
-      <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
-        <div className="flex flex-col bg-white p-8 rounded-lg space-y-4">
-          <h1 className="text-4xl font-bold">Accept Buy Offer</h1>
-          <button
-          className="bg-black text-white p-4 rounded-lg"
-          onClick={async () => {
-            setAccepting(true);
-            console.log('Buy Offer : ', buyOffer)
-            try {
-              const tx = {
-                "TransactionType": "NFTokenAcceptOffer",
-                "Account": userWallet.classicAddress,
-                "NFTokenBuyOffer": buyOffer,
-              }
-              const submitted_tx = await client.submitAndWait(tx, {
-                autofill: true,
-                wallet: userWallet,
-              })
-              console.log('Accept Offer : ', submitted_tx);
-            } catch (error) {
-              console.log(error);
-            } finally {
-              try {
-                await createUserSellOffer();
-              } catch (error) {
-                console.log(error);
-              } finally {
-                setPrice('');
-              }
-              setAccepting(false);
-              setBuyOfferCreated(false);
-              getAllNFTS({ address: userWallet?.classicAddress });
-            }
-          }}
-          >
-            {accepting ? 'Accepting...' : 'Accept the Buy'}
-          </button>
-          <button
-          className="bg-black text-white p-4 rounded-lg"
-          onClick={() => setBuyOfferCreated(false)}
-          >
-            Cancel
-          </button>
-        </div>
-      </div>
-    )}
+      )}
     </>
   );
 }
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,11 +5,6 @@ import { useState, useEffect } from "react";
 import ShowNFTS from "./../components/ShowNFTS";
 import { Client, Wallet, convertStringToHex } from "xrpl";
 
-// const api = new RippleAPI({ server: "wss://s.altnet.rippletest.net:51233/" });
-const address = "rZTbKwGBRf9vDw7QMpTCoPym4mn2r6Ui3";
-const secret = 'sEdTDxhkFNtcKuQsuViju4yNJgCpNBC';
-const sequence = '46069813';
-
 function Profile({ seed }) {
   const [client] = useState(new Client("wss://s.altnet.rippletest.net:51233", { connectionTimeout: 5000 }));
   let userWallet;
@@ -21,6 +16,7 @@ function Profile({ seed }) {
   const [tokens, setTokens] = useState();
   const [publicAdress, setPublicAdress] = useState();
   const [balance, setBalance] = useState();
+  const [isModalOpen, setModalOpen] = useState();
 
   const [selectedFile, setSelectedFile] = useState();
   const changeHandler = (event) => {
@@ -95,87 +91,108 @@ function Profile({ seed }) {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between">
 
+      <div className="relative bg-white p-6 rounded-xl shadow-xl">
+        <div className="flex flex-col items-center justify-center space-y-4">
+          <div className="flex flex-col items-center p-10">
 
-      {/* <div className="fixed flex items-center justify-center">
-        <div className="relative w-auto max-w-md mx-auto my-6">
-          <div className="relative bg-white p-6 rounded-xl shadow-xl">
-            <div className="flex flex-col items-center justify-center space-y-4">
-              <div className="flex flex-col items-center p-10"> */}
+            <h1 className="text-4xl font-bold">Profile</h1>
+            <div className="my-5 space-y-4">
+              <div>
+                <h1 className="text-xl font-bold">Address :</h1>
+                <h1 className="text-xl">{publicAdress}</h1>
+              </div>
+              <div>
+                <h1 className="text-xl font-bold">Balance :</h1>
+                <h1 className="text-xl">{balance}</h1>
+              </div>
+            </div>
 
+            <button
+              className="bg-black text-white p-4 rounded-lg"
+              onClick={() => {
+                setModalOpen(true);
+              }}>
+              Create your NFT
+            </button>
 
-                <h1 className="text-4xl font-bold">Profile</h1>
-                <div className="space-y-12">
-                  <div>
-                    <h1 className="text-4xl font-bold">Address :</h1>
-                    <h1 className="text-4xl font-bold">{ publicAdress }</h1>
-                  </div>
-                  <div>
-                    <h1 className="text-4xl font-bold">Balance :</h1>
-                    <h1 className="text-4xl font-bold">{ balance }</h1>
-                  </div>
-                </div>
-                <div className="flex flex-col space-y-8">
-                  <label className="text-4xl font-bold">Choose file</label>
-                  <div className="flex flex-row space-y-4">
-                    <input type="file" disabled={uploadedFile} onChange={changeHandler} />
+            {isModalOpen ? (
+
+              <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+                <div className="flex flex-col bg-white p-8 rounded-lg space-y-4">
+                  <div className="flex flex-col space-y-8">
+                    <label className="flex flex-col text-4xl font-bold items-center">Choose file</label>
+                    <input title="Choose your file" className="w-30 h-8 bg-gray-200 rounded-lg file:border-0 hover:file:cursor-pointer file:mr-4 file:py-2 file:px-4 file:rounded-md file:text-white file:bg-black" type="file" disabled={uploadedFile} onChange={changeHandler} />
                     <button onClick={submitIPFS} disabled={uploadedFile} className="bg-black text-white p-4 rounded-lg">
-                      {uploadingFile}
+                      {uploadingFile === 'Uploading...' ? (<div><span className="inline-block h-3 w-3 animate-spin rounded-full mb-0.5 mr-1 border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+                        role="status">
+                      </span>{uploadingFile}</div>) : uploadingFile}
+                    </button>
+                    {uploadedFile && (
+                      <img
+                        src={`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${cid}`}
+                        className="w-64 h-64"
+                        alt="ipfs image"
+                      />
+                    )}
+                    <input className="outline-none border-2 border-gray-300 rounded-md" type='text' placeholder='NFT Name' value={nftName} onChange={(e) => setNftName(e.target.value)} />
+                    {sending ? (<button className="bg-black text-white p-4 rounded-lg">
+                      <span className="inline-block h-4 w-4 animate-spin rounded-full mb-0.5 mr-1 border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+                        role="status">
+                      </span>Creating NFT...
+                    </button>)
+                      : (
+                        cid && nftName ? (
+                          <button
+                            className="bg-black text-white p-4 rounded-lg"
+                            onClick={async () => {
+                              setSending(true);
+                              const minterWallet = Wallet.fromSeed(seed);
+                              const tx = {
+                                "TransactionType": "NFTokenMint",
+                                "Account": minterWallet?.classicAddress,
+                                "Flags": 8,
+                                "URI": convertStringToHex(`${nftName}|${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${cid}`),
+                                "NFTokenTaxon": 0,
+                              }
+                              console.log('minter : ', minterWallet);
+                              const submitted_tx = await client.submitAndWait(tx, {
+                                autofill: true,
+                                wallet: minterWallet,
+                              })
+                              console.log('Create Offer : ', submitted_tx);
+                              setSending(false);
+                              getAllNFTS({ address: minterWallet?.classicAddress });
+                              setNftName('');
+                              setUploadedFile(false);
+                              setUploadingFile('Upload to IPFS');
+                              setSelectedFile();
+                              setCid('');
+                            }}
+                          >Create NFT</button>
+                        ) :
+                          (<button className="bg-black text-white p-4 rounded-lg opacity-50" disabled>Upload a file then enter a name...</button>)
+                      )}
+                    <button
+                      className="bg-red-500 text-white p-4 rounded-lg"
+                      onClick={() => {
+                        setModalOpen(false);
+                      }}
+                    >
+                      Cancel
                     </button>
                   </div>
-                  {uploadedFile && (
-                    <img
-                      src={`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${cid}`}
-                      className="w-64 h-64"
-                      alt="ipfs image"
-                    />
-                  )}
-                  <input type='text' placeholder='NFT Name' value={nftName} onChange={(e) => setNftName(e.target.value)} />
-                  {sending ? (<button className="bg-black text-white p-4 rounded-lg">Creating NFT...</button>)
-                    : (
-                      cid && nftName ? (
-                        <button
-                          className="bg-black text-white p-4 rounded-lg"
-                          onClick={async () => {
-                            setSending(true);
-                            const minterWallet = Wallet.fromSeed(seed);
-                            const tx = {
-                              "TransactionType": "NFTokenMint",
-                              "Account": minterWallet?.classicAddress,
-                              "Flags": 8,
-                              "URI": convertStringToHex(`${nftName}|${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${cid}`),
-                              "NFTokenTaxon": 0,
-                            }
-                            console.log('minter : ', minterWallet);
-                            const submitted_tx = await client.submitAndWait(tx, {
-                              autofill: true,
-                              wallet: minterWallet,
-                            })
-                            console.log('Create Offer : ', submitted_tx);
-                            setSending(false);
-                            getAllNFTS({ address: minterWallet?.classicAddress });
-                            setNftName('');
-                            setUploadedFile(false);
-                            setUploadingFile('Upload to IPFS');
-                            setSelectedFile();
-                            setCid('');
-                          }}
-                        >Create NFT</button>
-                      ) :
-                        (<button className="bg-black text-white p-4 rounded-lg opacity-50" disabled>Upload a file then enter a name...</button>)
-                    )}
-                  <ShowNFTS tokens={tokens} getAllNFTS={getAllNFTS} userSeed={seed} />
                 </div>
+              </div>
+            ) : null}
 
+            <ShowNFTS tokens={tokens} getAllNFTS={getAllNFTS} userSeed={seed} />
 
-              {/* </div>
-            </div>
           </div>
-        </div> */}
+        </div>
+      </div>
 
-
-      {/* </div> */}
       <Footer />
     </main>
   );
@@ -186,7 +203,7 @@ export default function ProfilePage() {
   const [seed, setSeed] = useState('');
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-14">
       {seed ? (
         <Profile seed={seed} />
       ) : (
@@ -197,7 +214,7 @@ export default function ProfilePage() {
               <div className="flex flex-col items-center justify-center space-y-4">
                 <div className="flex flex-col items-center p-10">
                   <h1 className="text-2xl font-semibold mb-4">Connect your wallet</h1>
-                  <input type="text" placeholder="Your Seed" value={userInput} onChange={(e) => setUserInput(e.target.value)} className="mt-1 mb-6 p-2 block w-full border border-gray-300 rounded-md shadow-sm" />
+                  <input type="text" placeholder="Your Seed" value={userInput} onChange={(e) => setUserInput(e.target.value)} className="mt-1 mb-6 p-2 block w-full border border-gray-300 rounded-md shadow-sm outline-none" />
                   <button onClick={() => setSeed(userInput)} className="bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600">Load Wallet</button>
                 </div>
               </div>
